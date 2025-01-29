2b65fe2425584873ba06f26e4fa98baced3f42a1
eoin-kins <eoinkinsella@live.ie>
2025-01-29 14:46:14 +0000

    responsive image swap

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,13 @@ function Home() {
     <>
       <TopNav />
 
-      <Image fluid src="/hero-section.webp" loading="lazy" />
+      <Image
+        fluid
+        src="/hero-mobile.webp"
+        srcSet="/hero-mobile.webp 300w, /hero-section.webp 600w"
+        sizes="(max-width: 300px) 300px, 600px"
+        loading="lazy"
+      />
 
       <div className="follow-us">
         <h1>Follow our journey</h1>
